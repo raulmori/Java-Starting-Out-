ae5f62dcb1c0e81ed8c9d2dd2f330a599a06ae2c
raulmori <47406865+raulmori@users.noreply.github.com>
2022-02-03 18:34:52 -0500

    Create Lec6 Ex10(Imcomplete).java

---
+++ Lec6 Ex10(Imcomplete).java
@@ -0,0 +1,20 @@
+/**
+ Rectangle class, phase 5
+ */
+
+ public class Rectangle
+   {
+   private double length;
+   private double width;
+
+   /**
+   Constructor
+   @param len The length of the rectangle.
+   @param w The width of the rectangle.
+   */
+
+   public Rectangle(double len, double w)
+       {
+       length = len;
+       width = w;
+       }
