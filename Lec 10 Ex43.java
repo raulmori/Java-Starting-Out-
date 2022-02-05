f17a7697e4adae387c7dd43493a36770e196f82b
raulmori <47406865+raulmori@users.noreply.github.com>
2022-02-05 04:09:45 -0500

    Create Lec 10 Ex43.java

---
+++ Lec 10 Ex43.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+/**
+This program demonstrates an anonymous inner class.
+*/
+
+public class AnonymousClassDemo
+  {
+  public static void main(String[] args)
+      {
+      int num;
+
+      // Create a Scanner object for keyboard input.
+      Scanner keyboard = new Scanner(System.in);
+
+      // Create an object that implements IntCalculator.
+      IntCalculator square = new IntCalculator() {
+          public int calculate(int number)
+          {
+          return number * number;
+          }};
+      
+
+      // Get a number from the user.
+      System.out.print("Enter an integer number: ");
+      num = keyboard.nextInt();
+
+      // Display the square of the number.
+      System.out.println("The square is " + square.calculate(num)); 
+      }
+  }
