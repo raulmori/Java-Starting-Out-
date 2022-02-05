b7cdfbaea8f0950a26ef586cc8fa8afe10d7ce0e
raulmori <47406865+raulmori@users.noreply.github.com>
2022-02-05 02:51:06 -0500

    Create Lec10 Ex6.java

---
+++ Lec10 Ex6.java
@@ -0,0 +1,13 @@
+public class SubClass1 extends SuperClass1
+  {
+  /**
+  Constructor
+  */
+
+  public SubClass1()
+      {
+      System.out.println("This is the " +
+       "subclass constructor.");
+      }
+  }
+
