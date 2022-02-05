849371ed98a7c27690c1ca73bbc5c934af3d7994
raulmori <47406865+raulmori@users.noreply.github.com>
2022-02-05 03:50:49 -0500

    Create Lec10 Ex36.java

---
+++ Lec10 Ex36.java
@@ -0,0 +1,17 @@
+ /**
+This class implements the Displayable
+interface, but does not override the
+default display method.
+*/
+
+public class Person implements Displayable
+  {
+  private String name;
+
+  // Constructor
+  public Person(String n)
+      {
+      name = n;
+      }
+  }
+
