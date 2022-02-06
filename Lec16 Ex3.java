9ad69223fcc0f2f44bfa41e51d27525c87f789c0
raulmori <47406865+raulmori@users.noreply.github.com>
2022-02-06 03:39:04 -0500

    Create Lec16 Ex3.java

---
+++ Lec16 Ex3.java
@@ -0,0 +1,12 @@
+/**
+This class demonstrates the Recursive.message method.
+*/
+
+public class RecursionDemo
+  {
+  public static void main(String[] args)
+      {
+      Recursive.message(5);
+      }
+
+  }
