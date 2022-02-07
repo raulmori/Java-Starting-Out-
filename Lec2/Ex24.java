ac0e793f67727c0c5ea08e0d35bc8cd345d08f84
raulmori <47406865+raulmori@users.noreply.github.com>
2022-02-07 01:09:21 -0500

    Create Ex24.java

---
+++ Lec2/Ex24.java
@@ -0,0 +1,15 @@
+// PROGRAM: Comment1.java
+// Written by Herbert Dorfmann
+// This program calculates company payroll
+
+public class Comment1
+  {
+  public static void main(String[] args)
+      {
+      double payRate; // Holds the hourly pay rate
+      double hours; // Holds the hours worked
+      int employeeNumber; // Holds the employee number
+
+      // The Remainder of This Program is Omitted.
+      }
+  }
