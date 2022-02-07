7d1ca7e45513b384955f5b1649ac1fb15ab194ac
raulmori <47406865+raulmori@users.noreply.github.com>
2022-02-07 18:49:09 -0500

    Update and rename Ex22.java to Ex22_EditDecimalVisualPane.java

--- Lec3/Ex22_EditDecimalVisualPane.java
+++ Lec3/Ex22_EditDecimalVisualPane.java
@@ -12,7 +12,7 @@ import javax.swing.JOptionPane;
        double monthlyPay = 5000.0;
        double annualPay = monthlyPay * 12;
 
-       JOptionPane.showMessageDialog(null,
-       String.format("Your annual pay is $%,.2f", annualPay));
+       JOptionPane.showMessageDialog(null, String.format("Your annual pay is $%,.2f", annualPay));
+       
       }
  }
